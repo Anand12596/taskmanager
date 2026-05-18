c81d38faff91c21620ea1947ba0026426ec225f3
Anand <anandthota708@gmail.com>
2026-05-18 16:50:48 +0530

    Initial backend setup

---
+++ tasks/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from users.models import User
+from projects.models import Project
+
+class Task(models.Model):
+
+    STATUS_CHOICES = (
+        ('TODO', 'To Do'),
+        ('IN_PROGRESS', 'In Progress'),
+        ('DONE', 'Done'),
+    )
+
+    PRIORITY_CHOICES = (
+        ('LOW', 'Low'),
+        ('MEDIUM', 'Medium'),
+        ('HIGH', 'High'),
+    )
+
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+
+    due_date = models.DateField()
+
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='TODO'
+    )
+
+    priority = models.CharField(
+        max_length=20,
+        choices=PRIORITY_CHOICES,
+        default='MEDIUM'
+    )
+
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+        related_name='tasks'
+    )
+
+    assigned_to = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='tasks'
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
